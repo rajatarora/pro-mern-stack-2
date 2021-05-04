f303d323969f8dd5d04bf7b37e7481296f6b8867
Rajat Arora <rajat@rajatarora.info>
2021-05-04 10:00:21 +0530

    Create the Hello World functionality as a React Component

--- public/App.js
+++ public/App.js
@@ -1,9 +1,23 @@
 "use strict";
 
-var continents = ['Africa', 'North America', 'South America', 'Asia', 'Australia', 'Europe'];
-var helloContinents = Array.from(continents, continent => "Hello, ".concat(continent, "!"));
-var message = helloContinents.join(' ');
-var element = /*#__PURE__*/React.createElement("div", {
-  id: "Outer Div"
-}, /*#__PURE__*/React.createElement("h1", null, message));
+/*
+This is a simple React Component. Every component must extend `React.Component` and must have a `render()` method.
+Without the `render()` method the component won't display anything on screen. The `render()` method returns JSX.
+
+The method should only return ONE outer element like <div>. If you need to return multiple elements, wrap them inside
+`<React.Fragment> </React.Fragment>` or simply `<> </>`
+ */
+class HelloWorld extends React.Component {
+  render() {
+    var continents = ['Africa', 'North America', 'South America', 'Asia', 'Australia', 'Europe'];
+    var helloContinents = Array.from(continents, continent => "Hello, ".concat(continent, "!"));
+    var message = helloContinents.join(' ');
+    return /*#__PURE__*/React.createElement("div", {
+      id: "Outer Div"
+    }, /*#__PURE__*/React.createElement("h1", null, message));
+  }
+
+}
+
+var element = /*#__PURE__*/React.createElement(HelloWorld, null);
 ReactDOM.render(element, document.getElementById('content'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 /*
-This is a simple JSX file that is translated into pure JS using babel, and whose output is used by index.html.
-The code snippet under `const element` looks like HTML but is in fact JSX.
+This is a simple React Component. Every component must extend `React.Component` and must have a `render()` method.
+Without the `render()` method the component won't display anything on screen. The `render()` method returns JSX.
+
+The method should only return ONE outer element like <div>. If you need to return multiple elements, wrap them inside
+`<React.Fragment> </React.Fragment>` or simply `<> </>`
  */
 
-const continents = ['Africa', 'North America', 'South America', 'Asia', 'Australia', 'Europe'];
-const helloContinents = Array.from(continents, continent => `Hello, ${continent}!`);
-const message = helloContinents.join(' ');
+class HelloWorld extends React.Component {
 
-const element = (
-    <div id="Outer Div">
-        <h1>{message}</h1>
-    </div>
-);
+    render() {
+        const continents = ['Africa', 'North America', 'South America', 'Asia', 'Australia', 'Europe'];
+        const helloContinents = Array.from(continents, continent => `Hello, ${continent}!`);
+        const message = helloContinents.join(' ');
+
+        return (
+            <div id="Outer Div">
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+
+}
+
+const element = <HelloWorld />
 
 ReactDOM.render(element, document.getElementById('content'));
